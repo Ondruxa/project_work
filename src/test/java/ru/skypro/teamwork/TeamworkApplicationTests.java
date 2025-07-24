6a4b67f7d30e4da8ca45565353d750137c640114
aleksandr08ign <140877693+aleksandr08ign@users.noreply.github.com>
2025-07-24 23:37:31 +0300

    work_1

---
+++ src/test/java/ru/skypro/teamwork/TeamworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.teamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
